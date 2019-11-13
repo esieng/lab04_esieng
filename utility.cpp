75ef10a941a3278039486b19b961463e41eb8b02
Elaine Sieng <esieng@cs.ucsb.edu>
2019-11-12 18:26:07 -0800

    implemented utility function - namof function()

--- utility.cpp
+++ utility.cpp
@@ -11,11 +11,20 @@
 // these functions
 
 bool isOdd(int x) { 
-  return false;  // REPLACE THIS STUB WITH REAL CODE
+  return !(x%2 == 0);
 }
+
 bool isEven(int x) { 
-  return false;  // REPLACE THIS STUB WITH REAL CODE
+  return (x%2 == 0);
 }
-bool isPrime(int x) { 
-  return false;  // REPLACE THIS STUB WITH REAL CODE
+
+bool isPrime(int x) {
+  if(x<2){
+	  return false;
+  }
+  for(int i=2; i<x;i++){
+	 if((x%i) == 0)
+		 return false;
+  }
+  return true;
 }
